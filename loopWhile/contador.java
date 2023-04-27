d3652c11f10dd58f53c82d05a0da67d8efd79879
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-04-27 14:29:22 -0300

    Create contador.java

---
+++ loopWhile/contador.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+         int contador = 1;
+         while (contador <= 50) {
+          System.out.println(contador);
+        contador++;
+        }    
+    }
+}
